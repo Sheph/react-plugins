b1a314b84d6b71d889ac0bb9e0eac4f9393fbc12
stas <sheffmail@mail.ru>
2018-12-18 12:16:03 +0300

    small fixes

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,15 @@
-const path = require('path')
-const HtmlWebPackPlugin = require("html-webpack-plugin");
-
-const htmlPlugin = new HtmlWebPackPlugin({
-  template: "./src/index.html",
-  filename: "./index.html"
-});
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: './src/index.js',
   output: {
     path: path.resolve('build'),
-    filename: "core.js"
+    filename: 'core.js'
   },
   module: {
     rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      },
       {
         test: /\.js$/,
         exclude: /node_modules/,
@@ -30,14 +19,14 @@ module.exports = {
         test: /\.css$/,
         use: [
           {
-            loader: "style-loader"
+            loader: 'style-loader'
           },
           {
-            loader: "css-loader",
+            loader: 'css-loader',
             options: {
               modules: true,
               importLoaders: 1,
-              localIdentName: "[name]_[local]_[hash:base64]",
+              localIdentName: '[name]_[local]_[hash:base64]',
               sourceMap: true,
               minimize: true
             }
@@ -46,7 +35,15 @@ module.exports = {
       }
     ]
   },
-  plugins: [htmlPlugin],
+  plugins: [
+    new HtmlWebPackPlugin({
+     template: './src/index.html',
+     filename: './index.html'
+    }),
+    new webpack.ProvidePlugin({
+      'window.react': 'react'
+    })
+  ],
   devServer: {
     overlay: true
   }
