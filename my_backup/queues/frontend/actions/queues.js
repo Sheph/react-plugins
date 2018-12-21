b7b7e6275de653be7a536a6ef3fe132dfdd9de22
stas <sheffmail@mail.ru>
2018-12-21 19:32:04 +0300

    queues stuff

--- my_backup/queues/frontend/actions/queues.js
+++ my_backup/queues/frontend/actions/queues.js
@@ -3,11 +3,28 @@ import {postRequest} from 'acore/action';
 const QUEUES_URL = '/queues';
 
 export const QUEUES_ZONES_LISTED = 'QUEUES_ZONES_LISTED';
+export const QUEUES_REPORT_UPDATED = 'QUEUES_REPORT_UPDATED';
+export const QUEUES_REPORT_RESET = 'QUEUES_REPORT_RESET';
 
 export function zonesList() {
     const request = postRequest(`${QUEUES_URL}/data`, {});
     return {
         type: QUEUES_ZONES_LISTED,
         payload: request
+    };
+}
+
+export function reportUpdate(query) {
+    const request = postRequest(`${QUEUES_URL}/chart-data${query}`, {});
+    return {
+        type: QUEUES_REPORT_UPDATED,
+        payload: request
+    };
+}
+
+export function reportReset() {
+    return {
+        type: QUEUES_REPORT_RESET,
+        payload: null
     };
 }
--- my_backup/queues/frontend/containers/Main.js
+++ my_backup/queues/frontend/containers/Main.js
@@ -1,23 +1,31 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
+import {Link, withRouter} from 'react-router-dom';
 import withStyles from '@material-ui/core/styles/withStyles';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 import mainStyle from '../assets/styles/mainStyle.js';
 import ZoneCard from 'acore/containers/ZoneCard';
 import {peopleZoneImages} from 'acore/assets';
-import {zonesList} from '../actions/queues';
+import {zonesList, reportUpdate, reportReset} from '../actions/queues';
 
 class Main extends Component {
     constructor(props) {
         super(props);
-        this.handleBuildReport = this.handleBuildReport.bind(this);
+        this.state = {
+            checkedZones: {}
+        };
     }
 
     componentDidMount() {
         this.timeout = null;
         this.props.onZonesList();
+        if (this.props.location.search.length > 0) {
+            this.props.onReportUpdate(this.props.location.search);
+        } else {
+            this.props.onReportReset();
+        }
     }
 
     componentWillUnmount() {
@@ -28,6 +36,13 @@ class Main extends Component {
     }
 
     componentDidUpdate(prevProps, prevState) {
+        if (prevProps.location !== this.props.location) {
+            if (this.props.location.search.length > 0) {
+                this.props.onReportUpdate(this.props.location.search);
+            } else {
+                this.props.onReportReset();
+            }
+        }
         if (prevProps.queuesState.zones !== this.props.queuesState.zones) {
             if (!this.timeout) {
                 this.timeout = setTimeout(function() {
@@ -38,11 +53,46 @@ class Main extends Component {
         }
     }
 
-    handleBuildReport() {
+    static getDerivedStateFromProps(props, state) {
+        let zones = {...state.checkedZones};
+
+        for (let guid in zones) {
+            if (!(guid in props.queuesState.zones)) {
+                delete zones[guid];
+            }
+        }
+
+        return {...state, checkedZones : zones};
+    }
+
+    handleZoneToggle(guid, checked) {
+        this.setState(state => {
+            let zones = {...state.checkedZones};
+
+            if (checked) {
+                zones[guid] = 1;
+            } else {
+                delete zones[guid];
+            }
+
+            return {...state, checkedZones : zones};
+        });
     }
 
     render() {
         const { classes, queuesState } = this.props;
+        let reportLink = 'queues?mode=halfhour&objects=' + Object.keys(this.state.checkedZones).join(',');
+        var today = new Date();
+        var dd = today.getDate();
+        var mm = today.getMonth() + 1;
+        var yyyy = today.getFullYear();
+        if (dd < 10) {
+            dd = '0' + dd;
+        }
+        if (mm < 10) {
+            mm = '0' + mm;
+        }
+        reportLink += '&date=' + dd + '.' + mm + '.' + yyyy;
         return (
             <React.Fragment>
                 <Grid container spacing={8} className={classes.grid}>
@@ -50,13 +100,17 @@ class Main extends Component {
                         let zone = queuesState.zones[guid];
                         return (
                             <Grid item key={guid}>
-                                <ZoneCard zoneImage={peopleZoneImages[Math.min(zone.count, 5)]} zoneText={zone.name} />
+                                <ZoneCard
+                                    zoneImage={peopleZoneImages[Math.min(zone.count, 5)]}
+                                    zoneText={zone.name}
+                                    onChange={(evt) => this.handleZoneToggle(guid, evt.target.checked)}
+                                    data-id={guid} />
                             </Grid>
                         );
                     })}
                 </Grid>
                 <br/>
-                <Button variant="contained" onClick={this.handleBuildReport}>Build report</Button>
+                <Button variant="contained" disabled={Object.keys(this.state.checkedZones).length === 0} component={Link} to={reportLink}>Build report</Button>
             </React.Fragment>
         );
     }
@@ -65,14 +119,17 @@ class Main extends Component {
 Main.propTypes = {
     classes: PropTypes.object.isRequired,
     queuesState: PropTypes.object.isRequired,
-    onZonesList: PropTypes.func.isRequired
+    onZonesList: PropTypes.func.isRequired,
+    onReportUpdate: PropTypes.func.isRequired,
+    onReportReset: PropTypes.func.isRequired,
+    location: PropTypes.object,
 };
 
 const mapDispatchToProps = (dispatch) => {
     return {
-        onZonesList: () => {
-            dispatch(zonesList());
-        }
+        onZonesList: () => dispatch(zonesList()),
+        onReportUpdate: (query) => dispatch(reportUpdate(query)),
+        onReportReset: () => dispatch(reportReset())
     };
 };
 
@@ -82,4 +139,4 @@ function mapStateToProps(state) {
     };
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(withStyles(mainStyle)(Main));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(withStyles(mainStyle)(Main)));
